cf0015e831db648ae2cb79b6e23f6d0104e1ed05
Depressed Humanist <depressedhumanist@gmail.com>
2021-12-07 21:28:59 +0300

    added functionality to configuration window

--- handlers/chat.go
+++ handlers/chat.go
@@ -5,11 +5,10 @@ import (
 	"fmt"
 	"github.com/gempir/go-twitch-irc/v2"
 	"github.com/gorilla/websocket"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strings"
-	"twitch-multichat/utils"
 )
 
 var upgrader = websocket.Upgrader{
@@ -17,42 +16,28 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
-type Config struct {
-	TwitchID     string   `json:"twitchID"`
-	TwitchSecret string   `json:"twitchSecret"`
-	Streamers    []string `json:"streamers"`
-}
-
 func WsHandler(w http.ResponseWriter, r *http.Request) {
-	b, err := ioutil.ReadFile("secrets.json")
-	// can file be opened?
-	if err != nil {
-		fmt.Print(err)
-	}
-	config := &Config{}
-	_ = json.Unmarshal(b, config)
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	twi := utils.NewTwitchClient(config.TwitchID, config.TwitchSecret)
-	chanIds := make([]string, len(config.Streamers))
-	avatarMap := make(map[string]string)
-	for i, v := range config.Streamers {
-		user := twi.GrabUserInfo(http.DefaultClient, v)
-		//todo handle channel not found
-		avatarMap[strings.ToLower(user.Name)] = user.ProfilePic
-		chanIds[i] = user.ID
+	config := &Config{}
+
+	if data, err := os.ReadFile("config.json"); err == nil {
+		err = json.Unmarshal(data, &config)
+		if err != nil {
+			http.Error(w, "bad request", 400)
+			return
+		}
 	}
-	bttvEmotes := utils.GetBetterTTVEmotes(chanIds...)
-	log.Println(bttvEmotes)
+
 	client := twitch.NewAnonymousClient()
 	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
 		text := message.Message
 		words := strings.Split(text, " ")
 		for i, v := range words {
-			link, ok := bttvEmotes[v]
+			link, ok := config.BetterTTVEmotes[v]
 			if ok {
 				words[i] = `<img src="` + link + `"  width="25" height="25">`
 			}
@@ -62,7 +47,7 @@ func WsHandler(w http.ResponseWriter, r *http.Request) {
 			text = strings.Replace(text, v.Name, fmt.Sprintf(`<img src="https://static-cdn.jtvnw.net/emoticons/v2/%s/static/dark/1.0" width="25" height="25">`, v.ID), v.Count)
 		}
 		message.Message = text
-		message.Channel = `<img src="` + avatarMap[strings.ToLower(message.Channel)] + `" width="25" height="25">`
+		message.Channel = `<img src="` + config.AvatarMap[strings.ToLower(message.Channel)] + `" width="25" height="25">`
 		err = conn.WriteJSON(message)
 		if err != nil {
 			log.Println(err.Error())
--- handlers/index.go
+++ handlers/index.go
@@ -1,15 +1,86 @@
 package handlers
 
 import (
+	"encoding/json"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"strings"
+	"twitch-multichat/utils"
 )
 
+type Config struct {
+	TwitchID        string            `json:"twitchID"`
+	TwitchSecret    string            `json:"twitchSecret"`
+	Streamers       []string          `json:"streamers"`
+	SkipInit        bool              `json:"skipInit"`
+	AvatarMap       map[string]string `json:"avartarMap"`
+	BetterTTVEmotes map[string]string `json:"betterTTVEmotes"`
+}
+
 func Index(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	log.Println(r.Method)
+	config := &Config{}
+
+	if data, err := os.ReadFile("config.json"); err == nil {
+		err = json.Unmarshal(data, &config)
+		if err != nil {
+			http.Error(w, "bad request", 400)
+			return
+		}
+	}
+	if r.Method == "GET" {
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "bad request", 400)
+			return
+		}
+		if _, ok := r.Form["force"]; !ok && config.SkipInit {
+			http.Redirect(w, r, "/chat", 302)
+		}
+		files, err := template.ParseFiles("templates/index.html")
+		if err != nil {
+			http.Error(w, "can't read template "+err.Error(), 500)
+			return
+		}
+		err = files.Execute(w, config)
+		if err != nil {
+			http.Error(w, "can't execute template", 500)
+			return
+		}
 		return
 	}
-	http.ServeFile(w, r, "templates/index.html")
+	if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "bad request", 400)
+			return
+		}
+		if streamers, ok := r.Form["streamer"]; ok {
+			config.Streamers = streamers
+		}
+		_, ok := r.Form["skipInit"]
+		config.SkipInit = ok
+
+		twi := utils.NewTwitchClient(config.TwitchID, config.TwitchSecret)
+		chanIds := make([]string, len(config.Streamers))
+		config.AvatarMap = make(map[string]string)
+		for i, v := range config.Streamers {
+			user := twi.GrabUserInfo(http.DefaultClient, v)
+			//todo handle channel not found
+			config.AvatarMap[strings.ToLower(user.Name)] = user.ProfilePic
+			chanIds[i] = user.ID
+		}
+		config.BetterTTVEmotes = utils.GetBetterTTVEmotes(chanIds...)
+
+		data, _ := json.Marshal(config)
+		err = os.WriteFile("config.json", data, os.ModePerm)
+		if err != nil {
+			http.Error(w, "can't write config", 500)
+			return
+		}
+		http.Redirect(w, r, "/chat", 302)
+		return
+	}
 }
